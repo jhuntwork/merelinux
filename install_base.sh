d0d8a5a1d40196d084a72e56969973f415c00e52
jhuntwork <jhuntwork@lightcubesolutions.com>
2010-04-15 14:41:01 +0000

    Finalize listing of initial base packages

--- install_base.sh
+++ install_base.sh
@@ -1,50 +1,65 @@
 #!/bin/bash
 
-# Command to install all base system packages at once
 ARCH=$(uname -m)
-rpm -ivh \
- /usr/src/rpm/RPMS/$ARCH/base-layout-0.1-3.$ARCH.rpm \
- /usr/src/rpm/RPMS/noarch/base-files-0.0-1.noarch.rpm \
- /usr/src/rpm/RPMS/$ARCH/glibc-2.11-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/readline-6.0-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/bash-4.0-3.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/bash-doc-4.0-3.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/perl-5.10.1-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/zlib-1.2.3-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/ncurses-5.7-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/texinfo-4.13-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/gcc-libs-4.4.2-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/gcc-c++-libs-4.4.2-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/coreutils-7.6-2.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/e2fsprogs-1.41.8-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/grep-2.5.4-2.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/inetutils-1.6-2.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/procps-3.2.8-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/sed-4.2.1-2.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/util-linux-ng-2.16-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/bzip2-1.0.5-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/diffutils-2.8.1-2.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/file-5.03-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/gawk-3.1.7-2.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/findutils-4.4.2-2.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/groff-1.20.1-2.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/gzip-1.3.13-2.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/db-4.7.25-2.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/iproute2-2.6.29-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/kbd-1.15-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/less-429-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/module-init-tools-3.10-2.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/psmisc-22.8-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/gdbm-1.8.3-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/man-db-2.5.5-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/noarch/man-pages-3.23-1.noarch.rpm \
- /usr/src/rpm/RPMS/noarch/iana-etc-2.30-1.noarch.rpm \
- /usr/src/rpm/RPMS/$ARCH/patch-2.5.9-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/Python-2.6.3-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/cracklib-2.8.13-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/Linux-PAM-1.1.0-2.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/shadow-4.1.4.2-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/sysklogd-1.5-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/sysvinit-2.86-1.$ARCH.rpm \
- /usr/src/rpm/RPMS/$ARCH/tar-1.22-1.$ARCH.rpm
+REPOS="/usr/src/rpm/RPMS"
+EXTRA="-r /mnt/tempsystem"
 
+# Command to install all base system packages at once
+rpm $EXTRA --initdb
+rpm -qa $EXTRA
+rpm -ivh $EXTRA \
+ $REPOS/$ARCH/base-layout-0.1-5.$ARCH.rpm \
+ $REPOS/noarch/base-files-0.1-1.noarch.rpm \
+ $REPOS/$ARCH/glibc-2.11.1-2.$ARCH.rpm \
+ $REPOS/$ARCH/readline-6.1-2.$ARCH.rpm \
+ $REPOS/$ARCH/bash-4.1-3.$ARCH.rpm \
+ $REPOS/$ARCH/zlib-1.2.4-2.$ARCH.rpm \
+ $REPOS/$ARCH/ncurses-5.7-2.$ARCH.rpm \
+ $REPOS/$ARCH/perl-5.10.1-1.$ARCH.rpm \
+ $REPOS/$ARCH/texinfo-4.13a-1.$ARCH.rpm \
+ $REPOS/$ARCH/bash-doc-4.1-3.$ARCH.rpm \
+ $REPOS/$ARCH/gcc-libs-4.4.3-2.$ARCH.rpm \
+ $REPOS/$ARCH/gcc-c++-libs-4.4.3-2.$ARCH.rpm \
+ $REPOS/$ARCH/coreutils-8.4-2.$ARCH.rpm \
+ $REPOS/$ARCH/gmp-5.0.1-2.$ARCH.rpm \
+ $REPOS/$ARCH/e2fsprogs-1.41.11-1.$ARCH.rpm \
+ $REPOS/$ARCH/grep-2.6.1-1.$ARCH.rpm \
+ $REPOS/$ARCH/inetutils-1.7-1.$ARCH.rpm \
+ $REPOS/$ARCH/procps-3.2.8-1.$ARCH.rpm \
+ $REPOS/$ARCH/sed-4.2.1-2.$ARCH.rpm \
+ $REPOS/$ARCH/util-linux-ng-2.17.2-1.$ARCH.rpm \
+ $REPOS/$ARCH/bzip2-1.0.5-2.$ARCH.rpm \
+ $REPOS/$ARCH/diffutils-2.9-1.$ARCH.rpm \
+ $REPOS/$ARCH/file-5.04-1.$ARCH.rpm \
+ $REPOS/$ARCH/gawk-3.1.7-2.$ARCH.rpm \
+ $REPOS/$ARCH/findutils-4.4.2-2.$ARCH.rpm \
+ $REPOS/$ARCH/groff-1.20.1-2.$ARCH.rpm \
+ $REPOS/$ARCH/gzip-1.4-1.$ARCH.rpm \
+ $REPOS/$ARCH/iproute2-2.6.33-2.$ARCH.rpm \
+ $REPOS/$ARCH/kbd-1.15.1-1.$ARCH.rpm \
+ $REPOS/$ARCH/less-436-1.$ARCH.rpm \
+ $REPOS/$ARCH/gdbm-1.8.3-1.$ARCH.rpm \
+ $REPOS/$ARCH/flex-2.5.35-1.x86_64.rpm \
+ $REPOS/noarch/man-pages-3.24-1.noarch.rpm \
+ $REPOS/noarch/iana-etc-2.30-1.noarch.rpm \
+ $REPOS/$ARCH/patch-2.6.1-1.$ARCH.rpm \
+ $REPOS/$ARCH/psmisc-22.10-2.$ARCH.rpm \
+ $REPOS/$ARCH/sysklogd-1.5-1.$ARCH.rpm \
+ $REPOS/$ARCH/tar-1.23-1.$ARCH.rpm \
+ $REPOS/$ARCH/cracklib-2.8.16-2.$ARCH.rpm \
+ $REPOS/$ARCH/Linux-PAM-1.1.1-1.$ARCH.rpm \
+ $REPOS/$ARCH/shadow-4.1.4.2-1.$ARCH.rpm \
+ $REPOS/$ARCH/module-init-tools-3.11.1-1.$ARCH.rpm \
+ $REPOS/$ARCH/man-db-2.5.7-1.$ARCH.rpm \
+ $REPOS/noarch/lfs-bootscripts-20100124-1.noarch.rpm \
+ $REPOS/$ARCH/beecrypt-4.2.1-1.$ARCH.rpm \
+ $REPOS/$ARCH/vim-7.2-1.$ARCH.rpm \
+ $REPOS/$ARCH/udev-151-1.$ARCH.rpm \
+ $REPOS/$ARCH/pcre-8.02-1.$ARCH.rpm \
+ $REPOS/$ARCH/popt-1.15-1.$ARCH.rpm \
+ $REPOS/$ARCH/openssl-1.0.0-1.$ARCH.rpm \
+ $REPOS/$ARCH/sysvinit-2.86-1.$ARCH.rpm \
+ $REPOS/$ARCH/grub-1.98-1.$ARCH.rpm \
+ $REPOS/$ARCH/expat-2.0.1-2.$ARCH.rpm \
+ $REPOS/$ARCH/neon-0.29.3-1.$ARCH.rpm \
+ $REPOS/$ARCH/rpm-5.1.9-2.$ARCH.rpm \
