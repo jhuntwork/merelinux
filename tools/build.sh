84f1cbfb542a7f8ecda022dea072a0ba538e16c0
Jeremy Huntwork <jhuntwork@lightcubesolutions.com>
2014-05-29 15:39:43 +0100

    Make build.sh more robust

--- tools/build.sh
+++ tools/build.sh
@@ -1,18 +1,44 @@
 #!/bin/sh -e
 ROOT=/tmp/root
+clean=0
 
-cleanup() {
-    printf "Executing cleanup\n"
-    umount "${ROOT}/dev" &&
-    umount "${ROOT}/proc" &&
-    umount "${ROOT}/sys" &&
-    rm -rf "${ROOT}"
+while getopts "cp:" arg ; do
+    case $arg in
+    	c) clean=1 ;;
+    	p) pkgdir="$OPTARG" ;;
+    esac
+done
+
+if [ -z "$pkgdir" ] ; then
+    printf "Package directory required\n"
+    exit 1
+fi
+
+is_mounted() {
+    awk {'print $2'} /proc/mounts | grep -q "^$@$"
+    return $?
 }
 
-trap cleanup INT EXIT
+cleanup() {
+    printf "Unmounting virtual file systems\n"
+    for mount in "${ROOT}/dev" "${ROOT}/proc" "${ROOT}/sys" ; do
+        if is_mounted "$mount" ; then
+            umount "$mount"
+        fi
+    done
+    if [ $1 -eq 1 ] ; then
+        printf "Cleaning %s\n" "$ROOT"
+        rm -rf "$ROOT"
+    fi
+}
+
+trap "cleanup ${clean}" INT EXIT
+
+# Before doing anything ensure the directory is clean
+cleanup 1
 
 install -d "${ROOT}/var/lib/pacman"
-pacman -Sy -c --noconfirm
+yes | pacman -Sy -cc
 pacman -Sy --noconfirm -r "${ROOT}" base-layout build-essential
 
 mount -t proc proc "${ROOT}/proc"
@@ -23,7 +49,7 @@ ln -s /proc/mounts "${ROOT}/etc/mtab"
 
 cp /etc/pacman.conf "${ROOT}/etc/"
 
-cp -a "$1" "${ROOT}/BUILD_PKG"
+cp -a "$pkgdir" "${ROOT}/BUILD_PKG"
 
 chroot "${ROOT}" /bin/sh -c "cd /BUILD_PKG && makepkg -fs --asroot --noconfirm"
 
