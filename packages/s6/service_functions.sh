b90a81ea870bfdc28b5e0c4ce538015a4de58e29
Jeremy Huntwork <jeremy@lightcubesolutions.com>
2015-08-11 15:01:10 +0000

    Improvements to s6
     * A better init using execline and setting up a fallback logging daemon
     * More user-friendly service management tool
     * Actually handle crash/finish/shutdown

--- packages/s6/service_functions.sh
+++ packages/s6/service_functions.sh
@@ -1,9 +1,13 @@
+N="\033[00m"
+SVCS_DIR="/run/service"
+AVAIL_DIR="/etc/s6-services/available"
+ENABL_DIR="/etc/s6-services/enabled"
+
 get_columns() {
     stty -a | grep -o columns\ [0-9]*\; | grep -o [0-9]*
 }
 
 display_ok() {
-    N="\033[00m"
     C="\033[32m"
     len=$(printf "$@" | wc -m)
     col=$(get_columns)
@@ -12,7 +16,6 @@ display_ok() {
 }
 
 display_fail() {
-    N="\033[00m"
     C="\033[31m"
     len=$(printf "$@" | wc -m)
     col=$(get_columns)
@@ -21,21 +24,85 @@ display_fail() {
 }
 
 usage() {
-    printf "Usage: %s [service_name] [start|stop|status|restart]\n" $0
+    USAGE="
+  Usage:
+    %s list
+    %s <service_name> [command]
+
+  Commands:
+    enable   Enables and attempts to start a service
+    disable  Disables and attempts to stop a service
+    start    Attempt to start a service
+    stop     Attempt to stop a service
+    status   Show status of a service
+    reload   Signal the service to reload its configuration
+    restart  Stop and then start a service
+
+"
+    printf "$USAGE" $0 $0
     exit 1
 }
 
 check_args() {
-    [ $# -ne 2 ] && usage
-    case "$2" in
-        start|stop|status|restart) break ;;
-        *) usage ;;
-    esac
+    if [ "$1" = "list" ] ; then
+        list_services
+    else
+        [ $# -ne 2 ] && usage
+        case "$2" in
+            enable|disable|start|stop|status|reload|restart)
+                svc=$1
+                act=$2
+                svcdir="${SVCS_DIR}/${svc}"
+                export svc act svcdir
+                break
+                ;;
+            *) usage ;;
+        esac
+    fi
+}
+
+is_supervised() {
+    if ! s6-svok $svcdir ; then
+        printf "%s is not a supervised service\n" ${svc}
+        exit 1
+    fi
 }
 
 is_service() {
-    if ! s6-svok "/service/${1}" ; then
-        printf "%s is not a supervised service\n" $1
+    if [ ! -x ${AVAIL_DIR}/${svc}/run ] ; then
+        printf "%s is not an available service\n" ${svc}
         exit 1
     fi
+}
+
+run_cmd() {
+    printf "%s" "$message"
+    if $1 ; then
+        display_ok "$message"
+    else
+        display_fail "$message"
+    fi
+}
+
+list_services() {
+    LIST="Service Enabled Status ------- ------- ------- "
+    for dir in $(find ${AVAIL_DIR} -type d -mindepth 1 -maxdepth 1 | sort | xargs) ; do
+        bn=${dir##*/}
+        LIST="$LIST ${bn} "
+        if [ -e "${ENABL_DIR}/${bn}" ] ; then
+            LIST="$LIST enabled "
+            status=$(s6-svstat -n ${SVCS_DIR}/${bn} | cut -d' ' -f1)
+            if echo $status | grep -q up ; then
+                LIST="$LIST up "
+            elif echo $status | grep -q down ; then
+                LIST="$LIST down "
+            else
+                LIST="$LIST unknown "
+            fi
+        else
+            LIST="$LIST disabled unknown "
+        fi
+    done
+    printf "%-30s%-30s%s\n" $LIST
+    exit 0
 }
