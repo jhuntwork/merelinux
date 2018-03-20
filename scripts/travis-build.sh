9b875626346c9895cf607d1c99aa428898d23a1f
Jeremy Huntwork <jhuntwork@lightcubesolutions.com>
2018-03-20 23:37:53 +0000

    Only pull build-essential if required

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -1,13 +1,15 @@
 #!/bin/sh -xe
 install -d staging merebuild/logs merebuild/sources merebuild/pkgs
 for file in $(git diff --name-only HEAD~1) ; do
+    be=0
     if printf '%s' "$file" | grep -q '^packages/'; then
         base="${file%/*}"
+        [ $be -eq 0 ] && docker pull mere/buildessential && be=1
         docker run \
             -v "$(pwd):/tmp/wd" \
             -v "$(pwd)/staging:/tmp/staging" \
             -v "$(pwd)/merebuild:/merebuild" \
             mere/build-essential \
-            "cd /tmp/wd/${base}; makepkg -Ls --noconfirm"
+            "cd /tmp/wd/${base} && pwd && makepkg -Ls --noconfirm"
     fi
 done
