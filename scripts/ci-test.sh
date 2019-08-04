0c97a0fbed19ab0dfc2f063596e173ee87a06b36
Jeremy Huntwork <jhuntwork@lightcubesolutions.com>
2019-08-03 21:05:51 -0400

    [pacman] Use pkgs.merelinux.org as main server

--- scripts/ci-test.sh
+++ scripts/ci-test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 # shellcheck disable=SC2154,SC1090
 . "$CIRCLE_WORKING_DIRECTORY"/.env
-if [ -n "$pkg" ] ; then
+if [ -n "$pkg" ] && [ -f "$pkg/PKGTEST" ]; then
   cd "$pkg"
   sh PKGTEST
 fi
