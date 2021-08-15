915c8c5e22593657961600e33c924e60b1a25925
Jeremy Huntwork <jhuntwork@lightcubesolutions.com>
2021-08-15 18:21:52 +0000

    Adjust CircleCi jobs
    The plain docker executor does not seem to allow you to specify a docker
    version. Instead, for commands relying on docker, use the machine
    executor which does allow this. Update ci-upload.sh to accomodate
    running on the machine executor.

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -2,26 +2,7 @@
 # shellcheck disable=SC2154,SC1090
 . "$CIRCLE_WORKING_DIRECTORY"/.env
 if [ -n "$pkg" ] ; then
-    pacman -Syu --noconfirm
-    pacman -Q
-    . "$pkg"/PKGBUILD
-    proposed="${pkgver}-${pkgrel}"
-    current="$(pacman -Sl | grep "${pkgname} " | cut -d' ' -f3)"
-    if [ "$(printf '%s\n' "$proposed" "$current" | \
-            sort -t. -n -k1,1 -k2,2 -k3,3 -k4,4 | \
-            tail -n1)" != "$proposed" ]; then
-        printf "The proposed version '%s' appears to be less \
-than the current version '%s'\n" "$proposed" "$current"
-        exit 1
-    fi
-    install -d /mere/logs /mere/sources
-    cd "$pkg"
-    sed -i '/MAKEFLAGS=/s@=.*@=@' /etc/makepkg.conf
-    makepkg -Ls --noconfirm
-    if [ "$(git rev-parse --abbrev-ref HEAD)" = 'master' ]; then
-        makepkg --allsource
-        mv ./*.src.tar.xz /tmp/staging
-    fi
+    ./scripts/build.sh "$pkg"
 else
     printf 'No packages are required to build in this commit.\n'
 fi
--- scripts/ci-upload.sh
+++ scripts/ci-upload.sh
@@ -4,7 +4,12 @@ if [ "$bn" != 'master' ]; then
     printf 'Skipping for branch: %s\n' "$bn"
     exit 0
 fi
-pacman -Syu --noconfirm python
+
+curl -LO http://pkgs.merelinux.org/stable/pacman-latest-x86_64.pkg.tar.xz
+tar -xf pacman-latest-x86_64.pkg.tar.xz
+install -d ./var/lib/pacman
+./bin/pacman -S --config etc/pacman.conf -y -r . --noconfirm --overwrite pacman-build
+
 pip install awscli
 
 install -d pkgs/testing
@@ -13,11 +18,11 @@ curl -fsL http://pkgs.merelinux.org/testing/main.db.tar.gz \
 curl -fsL http://pkgs.merelinux.org/testing/main.files.tar.gz \
     -o pkgs/testing/main.files.tar.gz
 
-sed -i '/bsdtar -xf .*dbfile/s@-C@--no-fflags -C@' /bin/repo-add
-find /tmp/staging -name "*.src.tar.xz" -exec mv '{}' pkgs/testing/ \;
-find /tmp/staging -name "*.pkg*" | while read -r file ; do
+sed -i '/bsdtar -xf .*dbfile/s@-C@--no-fflags -C@' bin/repo-add
+find "${HOME}/.mere/tmp/staging" -name "*.src.tar.xz" -exec mv '{}' pkgs/testing/ \;
+find "${HOME}/.mere/tmp/staging" -name "*.pkg*" | while read -r file ; do
     mv "$file" pkgs/testing
-    repo-add -R pkgs/testing/main.db.tar.gz "pkgs/testing/${file##*/}"
+    ./bin/repo-add -R pkgs/testing/main.db.tar.gz "pkgs/testing/${file##*/}"
 done
 
 aws s3 sync pkgs s3://pkgs.merelinux.org
