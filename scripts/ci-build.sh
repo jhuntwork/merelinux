0603609147bbf88ec45753114b8580938b58bb3a
Jeremy Huntwork <jhuntwork@lightcubesolutions.com>
2021-08-16 04:09:05 +0000

    Don't build packages on master pipeline
    We can use packages build in pull request pipelines

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -e
 # shellcheck disable=SC2154,SC1090
-. "$CIRCLE_WORKING_DIRECTORY"/.env
-if [ -n "$pkg" ] ; then
-    MEREDIR="$(pwd)/.mere" ./scripts/build.sh "$pkg"
-else
-    printf 'No packages are required to build in this commit.\n'
+bn="$(git rev-parse --abbrev-ref HEAD)"
+if [ "$bn" != 'master' ] ; then
+    . "$CIRCLE_WORKING_DIRECTORY"/.env
+    if [ -n "$pkg" ] ; then
+        MEREDIR="$(pwd)/.mere" ./scripts/build.sh "$pkg"
+    else
+        printf 'No packages are required to build in this commit.\n'
+    fi
 fi
